06cf1315f29aa8e00473506038e59c4da0b0750a
Jackie Tieu <jackietieu2@gmail.com>
2025-10-22 03:02:35 -0400

    less divs

--- src/lib/components/EvolutionGrid.svelte
+++ src/lib/components/EvolutionGrid.svelte
@@ -13,59 +13,43 @@
       Array.from({ length: gridSize }, (_, x) => ({ x, y }))
     )
   );
-
-  let scrollContainer: HTMLDivElement;
-
-  // Ensure scrolling works properly on mount
-  $effect(() => {
-    if (scrollContainer) {
-      // Small initial scroll to "activate" the scroll area
-      scrollContainer.scrollLeft = 1;
-      setTimeout(() => {
-        scrollContainer.scrollLeft = 0;
-      }, 10);
-    }
-  });
 </script>
 
-<div class="w-full flex justify-center">
+<div
+  class="w-full overflow-x-auto touch-pan-x flex justify-center"
+  style="-webkit-overflow-scrolling: touch; scroll-behavior: smooth;"
+>
   <div
-    class="overflow-x-auto touch-pan-x"
-    style="-webkit-overflow-scrolling: touch; scroll-behavior: smooth;"
-    bind:this={scrollContainer}
+    class="grid gap-0 p-4 relative"
+    style="grid-template-columns: repeat({gridSize}, 3.25rem); width: max-content; overflow: visible;"
   >
-    <div
-      class="grid gap-0 p-4 relative mx-auto"
-      style="grid-template-columns: repeat({gridSize}, 3.25rem); width: max-content; overflow: visible;"
-    >
-      {#each grid as row}
-        {#each row as cell}
-          {@const xBall = starterBalls[cell.x - 1]}
-          {@const yBall = starterBalls[cell.y - 1]}
-          {@const evolution = evolutions[xBall]?.[yBall]?.evolution}
-          {@const currentBall = cell.x === 0 ? yBall : cell.y === 0 ? xBall : evolution}
-          <GridItem x={cell.x} y={cell.y} ballKey={currentBall}>
-            {#if cell.x === cell.y}
-              <span style="color: var(--yellow); font-size: 2em;">X</span>
-            {:else if cell.x === 0}
-              <GridIcon
-                src={assetMap.ballIcons[yBall]}
-                alt={ballInformation[yBall]?.name + ' ball'}
-              />
-            {:else if cell.y === 0}
-              <GridIcon
-                src={assetMap.ballIcons[xBall]}
-                alt={ballInformation[xBall]?.name + ' ball'}
-              />
-            {:else if evolution}
-              <GridIcon
-                src={assetMap.ballIcons[evolution]}
-                alt={ballInformation[evolution]?.name + ' ball'}
-              />
-            {/if}
-          </GridItem>
-        {/each}
+    {#each grid as row}
+      {#each row as cell}
+        {@const xBall = starterBalls[cell.x - 1]}
+        {@const yBall = starterBalls[cell.y - 1]}
+        {@const evolution = evolutions[xBall]?.[yBall]?.evolution}
+        {@const currentBall = cell.x === 0 ? yBall : cell.y === 0 ? xBall : evolution}
+        <GridItem x={cell.x} y={cell.y} ballKey={currentBall}>
+          {#if cell.x === cell.y}
+            <span style="color: var(--yellow); font-size: 2em;">X</span>
+          {:else if cell.x === 0}
+            <GridIcon
+              src={assetMap.ballIcons[yBall]}
+              alt={ballInformation[yBall]?.name + ' ball'}
+            />
+          {:else if cell.y === 0}
+            <GridIcon
+              src={assetMap.ballIcons[xBall]}
+              alt={ballInformation[xBall]?.name + ' ball'}
+            />
+          {:else if evolution}
+            <GridIcon
+              src={assetMap.ballIcons[evolution]}
+              alt={ballInformation[evolution]?.name + ' ball'}
+            />
+          {/if}
+        </GridItem>
       {/each}
-    </div>
+    {/each}
   </div>
 </div>
